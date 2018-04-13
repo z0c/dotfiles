e3e0a78bba26009f44a482cd854dd563fabf4616
Nuno Pereira <nvnivs@gmail.com>
2018-04-13 14:37:16 +0100

    Added git update

--- install.sh
+++ install.sh
@@ -6,13 +6,17 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
 DOTFILES_EXTRA_DIR="$HOME/.extra"
 
+# Update myself
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then
+  git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+fi
+
 # Make utils available
 PATH="$DOTFILES_DIR/bin:$PATH"
 
 # Make utils executable
 find "$DOTFILES_DIR/bin/" -type f -exec chmod +x {} \;
 
-# TODO: Update myself
 # TODO: Bunch of symlinks
 
 # Installers

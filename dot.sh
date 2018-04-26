08d7e607e502931d2e4df59a35caa3d65e3c5b24
n p <nvnivs@gmail.com>
2018-04-26 20:50:46 +0100

    Changed method from ln to cp

--- dot.sh
+++ dot.sh
@@ -16,14 +16,8 @@ if is-executable git -a -d "$DOTFILES_DIR/.git"; then
   git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 fi
 
-# Bunch of symlinks
-ln -sfv "$DOTFILES_DIR/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/.bashrc" ~
-ln -sfv "$DOTFILES_DIR/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
-ln -sfv "$DOTFILES_DIR/.Xresources" ~
-ln -sfv "$DOTFILES_DIR/.xinitrc" ~
-ln -sfv "$DOTFILES_DIR/.config/i3/config" ~/.config/i3/
+# Copy profile dotfiles
+cp -r "$DOTFILES_DIR/profile/." ~
 
 # Installers
 . "$DOTFILES_DIR/install/pacman.sh"

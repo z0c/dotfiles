d6f914ac2a7bb8e7b77bcce90c70998181bcd548
n p <nvnivs@gmail.com>
2018-05-31 22:30:12 +0100

    Disabled docker on startup

--- install/docker.sh
+++ install/docker.sh
@@ -2,6 +2,6 @@ log-info 'Docker setup...'
 # Pack, ship and run any application as a lightweight container
 dot-info 'Docker setup'
 sudo pacman -S --needed --noconfirm --overwrite docker
-sudo systemctl enable docker && sudo systemctl start docker
+sudo systemctl disable docker
 sudo groupadd docker
 sudo usermod -aG docker $USER

fc10b0b3626578fc11acbb0e33a540c20e9bc2a0
n p <nvnivs@gmail.com>
2018-06-02 16:06:48 +0100

    Dont attempt to install 2.2.5 if already installed

--- install/rvm.sh
+++ install/rvm.sh
@@ -1,10 +1,12 @@
-if type "rvm" >/dev/null 2>&1; then
-    dot-info 'Rvm ruby 2.2.5'
-    rvm install 2.2.5
-else
+is-executable rvm || {
     dot-info 'Rvm install'
     curl -L get.rvm.io > /tmp/rvm-install
     bash < /tmp/rvm-install
-fi
+}
+
+rvm list |grep '2\.2\.5' >/dev/null 2>&1 || {
+    dot-info 'Rvm ruby 2.2.5'
+    rvm install 2.2.5
+}
 
 gem install vimgolf

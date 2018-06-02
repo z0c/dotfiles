9c02cee14e2bd32ae10670635a7ea37c946c909a
n p <nvnivs@gmail.com>
2018-06-02 15:33:53 +0100

    Re-ordered

--- install/pip.sh
+++ install/pip.sh
@@ -1,17 +1,15 @@
 dot-info 'Pip install or upgrade'
 sudo pip install --upgrade pip
 
+dot-info 'Pip install system packages'
 system_packages=(
     msgpack-python # Cache control dependency
 )
-
-dot-info 'Pip install system packages'
 pip install "${system_packages[@]}"
 
+dot-info 'Pip install user packages'
 user_packages=(
     markdown       # Markdown helper
 )
-
-dot-info 'Pip install user packages'
 pip install "${user_packages[@]}" --user
 

ff5e33861b54e61c8431c19ccf44a3b41749466b
n p <nvnivs@gmail.com>
2018-06-02 14:54:06 +0100

    Fixed issue with msgpack

--- install/pip.sh
+++ install/pip.sh
@@ -1,10 +1,17 @@
-packages=(
-    markdown       # Markdown helper
-    msgpack-python # Cache control dependency
-)
-
 dot-info 'Pip install or upgrade'
 sudo pip install --upgrade pip
 
-dot-info 'Pip install packages'
-pip install "${packages[@]}" --user
+system_packages=(
+    msgpack-python # Cache control dependency
+)
+
+dot-info 'Pip install system packages'
+pip install "${system_packages[@]}"
+
+user_packages=(
+    markdown       # Markdown helper
+)
+
+dot-info 'Pip install user packages'
+pip install "${user_packages[@]}" --user
+
